b2cee72d90ddd110b5d5d5c77c108d9118e5029b
mohammad kadour <moh.kadour2234@gmail.com>
2025-03-06 23:54:12 +0300

    new update

--- app.js
+++ app.js
@@ -1,52 +1,43 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
-
-const User = require("./models/user");
-// const Product = require("./models/product");
+const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById("67c61ca325d0daa827e422f7")
-    .then((user) => {
-      if (!user) {
-        console.log("User not found!");
-        return next(); // Move to next middleware without setting req.user
-      }
+  User.findById('67c61ca325d0daa827e422f7')
+    .then(user => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
-    .catch((err) => {
-      console.log("Error fetching user:", err);
-    });
+    .catch(err => console.log(err));
 });
 
-app.use("/admin", adminRoutes);
-
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
 app.use(errorController.get404);
 
-// Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-
-// User.hasMany(Product);
-
-mongoConnect((client) => {
+mongoConnect(() => { 
   app.listen(3000);
 });
+
+
+//67c61ca325d0daa827e422f7
 
 // sequelize
 //   .sync({ force: true })
--- controllers/admin.js
+++ controllers/admin.js
@@ -21,44 +21,17 @@ exports.postAddProduct = (req, res, next) => {
     null,
     req.user._id
   );
-  console.log('req.user',req.user,'req.user')
   product
     .save()
     .then(result => {
       // console.log(result);
       console.log('Created Product');
-      res.redirect('/admin/products');
+      res.redirect('/admin/products'); 
     })
     .catch(err => {
       console.log(err);
     });
 };
-
-
-// exports.postAddProduct = (req, res, next) => {
-//   const title = req.body.title;
-//   const imageUrl = req.body.imageUrl;
-//   const price = req.body.price;
-//   const description = req.body.description;
-//   const product = new Product(
-//     title,
-//     price,
-//     description,
-//     imageUrl,
-//     null,
-//     req.user._id
-//   );
-//   product
-//     .save()
-//     .then(result => {
-//       // console.log(result);
-//       console.log('Created Product');
-//       res.redirect('/admin/products');
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// };
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
